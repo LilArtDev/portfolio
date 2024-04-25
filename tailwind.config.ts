43e8a977889aa056132d051b151a9361e91aeca1
Arthur Fernandez <contatoarthurfernandez@gmail.com>
2024-04-25 02:16:24 -0300

    chore: project configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,34 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/hocs/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#6F01EC",
+        secondary: "#785FDC",
+        third: "#3F334B",
+        background: "#161618",
+        backgrondSec: "#303032",
+        backgroundThird: "#444446",
+        gray: "#b3b3b3",
+      },
+      fontFamily: {
+        sans: ["Arial", "Helvetica", "sans-serif"],
+      },
+      fontSize: {
+        xs: ".75rem", // 12px
+        sm: ".875rem", // 14px
+        base: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
+        "6xl": "3.75rem", // 60px
+        "7xl": "4.5rem", // 72px
       },
     },
   },
