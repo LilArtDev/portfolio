8478a613c9ab23d8e5f06838437869fc4cb38ad0
Arthur Fernandez <contatoarthurfernandez@gmail.com>
2024-05-30 23:03:01 -0300

    feat: text "developer" adjust

--- src/components/sections/work-experience/work-experience.constants.ts
+++ src/components/sections/work-experience/work-experience.constants.ts
@@ -6,7 +6,7 @@ export const WORK_EXPERIENCES_TIMELINE_CONTENT: Pick<
 >[] = [
   {
     headline: "2021 - 2024",
-    title: "FullStack Developer",
+    title: "Desenvolvedor FullStack",
     description: `Na PeerBR (Produto da holding Grupo GCB), desempenhei um papel essencial no desenvolvimento e manutenção das aplicações web e mobile. Contribuí de forma intensiva para a criação da segunda versão do aplicativo web PeerBR, utilizando React, Axios, Styled Components e Jest, assegurando uma performance aprimorada e uma experiência de usuário superior. Também tive uma participação significativa na primeira versão do aplicativo mobile, desenvolvido com React Native, Redux e Expo/EAS, focando em uma funcionalidade perfeita entre plataformas.
       No backend, implementei e administrei a segunda versão da API REST utilizando NestJS e PostgreSQL, integrando funcionalidades robustas e garantindo uma gestão eficiente dos dados. Trabalhei com serviços AWS, incluindo EC2, ECS, S3 e Docker, o que permitiu implantações contínuas e garantiu uma infraestrutura confiável.`,
   },
